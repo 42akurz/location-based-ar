3e560c336936f2b686ba2de194381d7d3e58eb40
42akurz <alexku0609@gmail.com>
2023-05-24 16:28:11 +0200

    work

---
+++ script.js
@@ -0,0 +1,47 @@
+window.onload = () => {
+  // const button = document.querySelector('button[data-action="change"]');
+  // button.innerText = '﹖';
+
+  let places = staticLoadPlaces();
+  renderPlaces(places);
+};
+
+function staticLoadPlaces() {
+  return [
+    {
+      text: "RedBox was clicked",
+      location: {
+        lat: "49.2026165925665",
+        lng: "9.243124311009371",
+      },
+      color: "red",
+      scale: "20 20 20",
+      position: "0 30 0",
+    },
+  ];
+}
+
+function renderPlaces(places) {
+  let scene = document.querySelector("a-scene");
+
+  places.forEach((place) => {
+    let latitude = place.location.lat;
+    let longitude = place.location.lng;
+
+    let model = document.createElement("a-box");
+    model.setAttribute(
+      "gps-new-entity-place",
+      `latitude: ${latitude}; longitude: ${longitude};`
+    );
+    model.setAttribute("color", place.color);
+    model.setAttribute("scale", place.scale);
+    model.setAttribute("position", place.position);
+
+    model.addEventListener("click", function () {
+      const div = document.querySelector(".instructions");
+      div.innerText = place.text;
+    });
+
+    scene.appendChild(model);
+  });
+}
